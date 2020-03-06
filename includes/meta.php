c601d77a9b54e579666a726b7b7f29e95d2ffff7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-03-06 11:03:00 -0500

    Updated constant references

--- includes/meta.php
+++ includes/meta.php
@@ -11,9 +11,9 @@ function research_enqueue_frontend_assets() {
 	$theme = wp_get_theme();
 	$theme_version = $theme->get( 'Version' );
 
-	wp_enqueue_style( 'style-child', MYPROJECT_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
+	wp_enqueue_style( 'style-child', RESEARCH_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
 
-	wp_enqueue_script( 'script-child', MYPROJECT_THEME_JS_URL . '/script.min.js', array( 'jquery', 'script' ), $theme_version, true );
+	wp_enqueue_script( 'script-child', RESEARCH_THEME_JS_URL . '/script.min.js', array( 'jquery', 'script' ), $theme_version, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'research_enqueue_frontend_assets', 11 );
