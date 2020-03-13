9d88460c0fa1bc84e14130bfb1e3f2db9379b162
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-03-13 13:27:40 -0400

    Added logic for news and basic styles

--- includes/cluster-functions.php
+++ includes/cluster-functions.php
@@ -47,7 +47,7 @@ function research_get_today_markup( $story ) {
 	$parsed_url = parse_url( $story['today_url'] );
 	$path = $parsed_url['path'];
 
-	preg_match( '/\/news\/(?P<slug>[a-zA-Z\-]+)/', $path, $matches );
+	preg_match( '/\/news\/(?P<slug>[a-zA-Z0-9\-]*)/', $path, $matches );
 
 	if ( isset( $matches['slug'] ) && ! empty( $matches['slug'] ) ) {
 		$slug = $matches['slug'];
@@ -64,7 +64,7 @@ function research_get_today_markup( $story ) {
 		$retval = isset( $item[0] ) && ! empty( $item[0] ) ? $item[0] : false;
 	}
 
-	return $retval;
+	return ! empty( $retval ) ? research_format_story( $retval, 'today' ) : '';
 }
 
 /**
@@ -88,8 +88,9 @@ function research_get_pegasus_markup( $story ) {
 		$url = 'https://www.ucf.edu/pegasus/wp-json/wp/v2/story/';
 
 		$param_string = http_build_query( array(
-			'limit' => 1,
-			'slug' => $slug
+			'limit'  => 1,
+			'slug'   => $slug,
+			'_embed' => true
 		) );
 
 		$item = research_fetch_cache_json( "$url?$param_string", 'research_cluster_news_item_' );
@@ -97,7 +98,7 @@ function research_get_pegasus_markup( $story ) {
 		$retval = isset( $item[0] ) && ! empty( $item[0] ) ? $item[0] : false;
 	}
 
-	return $retval;
+	return ! empty( $retval ) ? research_format_story( $retval, 'pegasus' ) : '';
 }
 
 /**
@@ -120,5 +121,68 @@ function research_get_research_story( $story ) {
  */
 function research_get_external_story( $story ) {
 	return false;
+}
+
+/**
+ * Formats the story and return the HTML markup
+ * @author Jim Barnes
+ * @since 1.0.0
+ * @param WP_Post $story The story post
+ * @return string
+ */
+function research_format_story( $story, $source = 'external' ) {
+	$thumbnail = research_get_thumbnail( $story, $source );
+
+	if ( in_array( $source, array( 'today', 'pegasus' ) ) ) {
+		$permalink = $story->link;
+		$title = $story->title->rendered;
+		$excerpt = $source === 'today' ? $story->excerpt->rendered : $story->story_description;
+		$source = $source === 'today' ? 'UCF Today' : 'Pegasus Magazine';
+	} else if ( $source = 'research' ) {
+		$permalink = get_permalink( $story->ID );
+		$title = $story->post_title;
+		$excerpt = $story->post_excerpt;
+		$source = 'UCF Research';
+	} else {
+		$permalink = $story['story_url'];
+		$title     = $story['story_title'];
+		$excerpt   = $story['story_excerpt'];
+		$source    = $story['story_source'];
+	}
+
+	ob_start();
+?>
+	<div class="ucf-news-item">
+		<a href="<?php echo $permalink; ?>">
+			<div class="ucf-news-thumbnail">
+				<img class="ucf-news-thumbnail-image image" src="<?php echo $thumbnail; ?>" alt>
+			</div>
+			<div class="ucf-news-item-content">
+				<div class="ucf-news-item-details">
+					<p class="ucf-news-item-title"><?php echo $title; ?></p>
+					<p class="ucf-news-item-excerpt"><?php echo $excerpt; ?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+<?php
+	return ob_get_clean();
+}
+
+function research_get_thumbnail( $story, $source = 'external' ) {
+	switch( $source ) {
+		case 'today':
+			return $story->thumbnail;
+		case 'pegasus':
+			$embedded = (array)$story->_embedded;
+			$featured_media = $embedded['wp:featuredmedia'];
+			return isset( $featured_media[0]->media_details->sizes->thumbnail->source_url ) ?
+				$featured_media[0]->media_details->sizes->thumbnail->source_url :
+				'';
+		case 'research':
+			return get_the_post_thumbnail_url( $story->ID );
+		default:
+			return $story['story_thumbnail'];
+	}
 }
 
