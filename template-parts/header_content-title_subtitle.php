7858a4a72bcef5503af3066babfd80f6f4752bc4
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-05-04 14:32:00 -0400

    Upadated styles on the clusters link

--- template-parts/header_content-title_subtitle.php
+++ template-parts/header_content-title_subtitle.php
@@ -14,8 +14,8 @@ $clusters_link = get_theme_mod( 'clusters_list_page' ) ? get_permalink( get_them
 	<div class="header-content-inner align-self-start pt-4 pt-sm-0 align-self-sm-center">
 		<div class="container">
 			<?php if ( $clusters_link ) : ?>
-			<div class="d-inline-block bg-inverse-t-1 py-3 px-2 mb-3">
-				<a class="text-primary text-uppercase letter-spacing-3 font-weight-black" href="<?php echo $clusters_link; ?>">UCF Faculty Clusters</a>
+			<div class="d-inline-block bg-inverse-t-2 p-3 mb-3">
+				<a class="text-inverse text-uppercase letter-spacing-3 font-weight-black" href="<?php echo $clusters_link; ?>">UCF Faculty Clusters</a>
 			</div>
 			<div class="clearfix"></div>
 			<?php endif; ?>
