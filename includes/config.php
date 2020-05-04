be13779639a62c8fc38b05c7ad1872c4412588ac
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-05-04 11:54:46 -0400

    Added link back to clusters list page and customizer option

--- includes/config.php
+++ includes/config.php
@@ -8,3 +8,39 @@ define( 'RESEARCH_THEME_STATIC_URL', RESEARCH_THEME_URL . '/static' );
 define( 'RESEARCH_THEME_CSS_URL', RESEARCH_THEME_STATIC_URL . '/css' );
 define( 'RESEARCH_THEME_JS_URL', RESEARCH_THEME_STATIC_URL . '/js' );
 define( 'RESEARCH_THEME_IMG_URL', RESEARCH_THEME_STATIC_URL . '/img' );
+define( 'RESEARCH_THEME_CUSTOMIZER_PREFIX', 'research_' );
+
+/**
+ * Defines sections used in the WordPress Customizer.
+ */
+function research_define_customizer_sections( $wp_customize ) {
+	$wp_customize->add_section(
+		RESEARCH_THEME_CUSTOMIZER_PREFIX . 'clusters',
+		array(
+			'title' => 'Clusters Settings'
+		)
+	);
+}
+
+add_action( 'customize_register', 'research_define_customizer_sections' );
+
+/**
+ * Defines settings and controls used in the WordPress Customizer.
+ */
+function research_define_customizer_fields( $wp_customize ) {
+	$wp_customize->add_setting(
+		'clusters_list_page'
+	);
+
+	$wp_customize->add_control(
+		'clusters_list_page',
+		array(
+			'type'        => 'dropdown-pages',
+			'label'       => 'Cluster Listing Page',
+			'description' => 'The page where all faculty cluster pages are listed',
+			'section'     => RESEARCH_THEME_CUSTOMIZER_PREFIX . 'clusters'
+		)
+	);
+}
+
+add_action( 'customize_register', 'research_define_customizer_fields' );
