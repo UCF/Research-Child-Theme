9189e87d244e6b335d3ed6bb7c6966d6f9e582ea
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2024-02-29 15:07:06 -0500

    Max height was defined

--- single-post.php
+++ single-post.php
@@ -1,7 +1,7 @@
 <?php get_header(); the_post(); ?>
 
 <div>
-	<?php the_post_thumbnail( 'large', array( 'class' => 'w-100 img-fluid text-center object-fit-cover' , 'style' => 'min-height: 500px') ) ?>
+	<?php the_post_thumbnail( 'large', array( 'class' => 'w-100 img-fluid text-center object-fit-cover' , 'style' => 'min-height: 500px; max-height: 700px') ) ?>
 </div>
 <article class="<?php echo $post->post_status; ?> post-list-item">
 	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
