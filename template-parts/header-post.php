236ba8a34ea1ec133f5dd5bcfd645fec5fe378be
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-03-11 16:52:40 -0400

    category-links were activated.

--- template-parts/header-post.php
+++ template-parts/header-post.php
@@ -11,21 +11,21 @@ $exclude_nav = get_field( 'page_header_exclude_nav', $obj );
 	<div class="row mb-4 pt-1">
 		<div class="col-5 my-1">
 		<?php
-			$selected_categories = wp_get_post_categories( get_the_ID() );
-			$count = count( $selected_categories );
-				if ( $selected_categories ) {
-					foreach ( $selected_categories as $index => $selected_category ) {
-						$category = get_category( $selected_category );
-		?>
-					<span class="medium my-3 p-1"><?php echo esc_html( $category->name ); ?><?php echo $index < $count - 1 ? ' | ' : ''; ?></span>
-		<?php
-					}
+		$selected_categories = wp_get_post_categories( get_the_ID() );
+		$count = count( $selected_categories );
+			if ( $selected_categories ) {
+				foreach ( $selected_categories as $index => $category_id ) {
+					$category = get_category( $category_id );
+					?>
+					<a href="<?php echo esc_url( get_category_link( $category_id ) ); ?>" class="my-3 p-1"><?php echo esc_html( $category->name ); ?></a><?php echo $index < $count - 1 ? ' |' : ''; ?>
+					<?php
 				}
+			}
 		?>
 	</div>
 		<div class="col-1"></div>
 		<div class="col-6 text-right my-1">
-			<span class="medium mb-2 p-1">Author: <?php echo $author; ?></span>
+			<span class="mb-2 p-1">Author: <?php echo $author; ?></span>
 		</div>
 	</div>
 </div>
